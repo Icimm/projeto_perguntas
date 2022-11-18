3f9d0bc8c935f508bd246a5fa228de6122f3627d
icaro <icarobellemskr@hotmail.com>
2022-11-17 21:44:49 -0300

    frist commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+runApp(PerguntaApp());
+}
+
+ class PerguntaApp extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá Flutter!!!'),
+      ),
+    );
+  }
+}
